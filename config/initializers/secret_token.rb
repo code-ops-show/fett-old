b390f95c8628ded82237c2ad224bed7fbb42a164
Sakchai Siripanyawuth <sakchai@artellectual.com>
2011-09-17 21:12:32 +0700

    - initial commit
    Signed-off-by: Sakchai Siripanyawuth <sakchai@artellectual.com>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fett::Application.config.secret_token = '62183539d0a569c1e0ac361ef32115df75a5146ee8adf4223dd56c59a5752acf90ec66ef412c9422b9dea3b6979aaad8e2f5763a960726551b5263344b5a025a'
